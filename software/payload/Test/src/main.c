4c9099e8d4036c03683effbb2df21e2afb866e1c
PatSmith328 <pjsmith29510@gmail.com>
2019-12-18 15:19:09 -0500

    I2C read

--- software/payload/Test/src/main.c
+++ software/payload/Test/src/main.c
@@ -20,6 +20,7 @@ void i2c_init(void);
 void delay(volatile uint32_t s);
 void uart_write(char* str);
 void i2c_write(uint8_t addr, uint8_t* data);
+void i2c_read(uint8_t addr, uint8_t* buf, uint8_t numbytes);
 
 void TIM5_IRQHandler(void)
 {
@@ -188,14 +189,6 @@ void i2c_init(void)
     I2C1->CCR |= I2C_CCR_FS | I2C_CCR_DUTY | (25) << I2C_CCR_CCR_Pos; // Sets the clock control such that fast mode is enabled with DUTY set to 1 and the time prescaler set to 25
     I2C1->TRISE |= (16) << I2C_TRISE_TRISE_Pos; // Programs a factor of 16 to relate the maximum rise time in Fast mode to the input clock
     I2C1->CR1 |= I2C_CR1_PE; // Enables I2C bus
-
-    /* Recieving
-     * Set Start bit
-     * Set ADDR
-     * Set DR
-     * Use RxNE bit to continuously recieve
-     * Set Stop bit
-     */
 }
 
 void delay(volatile uint32_t s)
@@ -211,11 +204,44 @@ void uart_write(char* str)
     for(uint8_t i = 0; i < strlen(str); i++)
     {
         USART2->DR = str[i];
-        while(!(USART2->SR & USART_SR_TC));
+        while(!(USART2->SR & USART_SR_TC)); // Waits until data byte is transmitted
     }
 }
 
 void i2c_write(uint8_t addr, uint8_t* data)
+{
+    /* Recieving
+     * Set Start bit
+     * Set ADDR
+     * Set DR
+     * Use RxNE bit to continuously recieve
+     * Set Stop bit
+     */
+
+    uint8_t read = 0;
+    I2C1->CR1 |= I2C_CR1_START; // Sends the start condition
+    if(I2C1->SR1 & I2C_SR1_SB)
+    {
+        I2C1->DR = addr | read; // Places the address into the data register to send
+        if(I2C1->SR1 & I2C_SR1_ADDR) // Checks SR1 register
+        {
+            if(I2C1->SR2 & I2C_SR2_TRA) // Checks SR2 register
+            {
+                for(uint8_t i = 0; i < sizeof(data); i++)
+                {
+                    I2C1->DR = data[i]; // Places each byte of data
+                    if(i == sizeof(data) - 1) // Conducts process 
+                    {
+                        I2C1->CR1 |= I2C_CR1_STOP; // Creates stop condition after last byte is transmitted
+                    }
+                    while(!(I2C1->SR1 & I2C_SR1_TXE)); // Waits until the data was sent
+                }
+            }
+        }
+    }
+}
+
+void i2c_read(uint8_t addr, uint8_t* buf, uint8_t numbytes)
 {
     /* Transmission
      * Set Start bit
@@ -225,23 +251,24 @@ void i2c_write(uint8_t addr, uint8_t* data)
      * Set Stop bit to end
      */
 
-    uint8_t read = 0;
-    I2C1->CR1 |= I2C_CR1_START; // Sends the start condition
+    uint8_t read = 1;
+    I2C1->CR1 |= I2C_CR1_START | I2C_CR1_ACK; // Sends the start condition and enables acknowledge return
     if(I2C1->SR1 & I2C_SR1_SB)
     {
         I2C1->DR = addr | read; // Places the address into the data register to send
-        if(I2C1->SR1 & I2C_SR1_ADDR)
+        if(I2C1->SR1 & I2C_SR1_ADDR) // Checks SR1 register
         {
-            if(I2C1->SR2 & I2C_SR2_TRA)
+            if(!(I2C1->SR2 & I2C_SR2_TRA)) // Checks SR2 register
             {
-                for(uint8_t i = 0; i < sizeof(data); i++)
+                for(uint8_t i = 0; i < numbytes; i++)
                 {
-                    I2C1->DR = data[i]; // Places each byte of data
-                    if(i == sizeof(data) - 1)
+                    while(!(I2C1->SR1 & I2C_SR1_RXNE)); // Waits until the data was recieved
+                    buf[i] = I2C1->DR; // Places each byte of data
+                    if(i == numbytes - 2) // Conducts the below process after reading the second to last byte
                     {
+                        I2C1->CR1 &= ~I2C_CR1_ACK; // Clears ACK bit
                         I2C1->CR1 |= I2C_CR1_STOP; // Creates stop condition after last byte is transmitted
                     }
-                    while(!(I2C1->SR1 & I2C_SR1_TXE)); // Waits until the data was sent
                 }
             }
         }
